9f1167757380a677c8fbd5779ab4730932d6f2df
Jonathan Ahrenkiel-Frellsen <jola190500@gmail.com>
2020-06-03 17:23:46 +0200

    Added sql database

---
+++ Appendix/Code/wiki_sql_database.sql
@@ -0,0 +1,38 @@
+/* entities eller hvad det nu hedder */
+CREATE TABLE article (
+    article_id int,
+    content TEXT,
+    title varchar(1000),
+    screaped_at DATE,
+    publish_date DATE,
+    PRIMARY KEY (article_id)
+);
+
+CREATE TABLE sources (
+    source_domain_id int,
+    source_domain varchar(255),
+    PRIMARY KEY (source_domain_id)
+);
+
+CREATE TABLE categories (
+    category_id int,
+    category varchar(1000),
+    PRIMARY KEY (category_id)
+);
+
+/* relations */
+CREATE TABLE source_to (
+    article_id int,
+    source_domain_id int,
+    PRIMARY KEY (article_id, source_domain_id),
+    FOREIGN KEY(article_id) References article,
+    FOREIGN KEY(source_domain_id) References sources
+);
+
+CREATE TABLE in_category (
+    article_id Int,
+    category_id int,
+    PRIMARY KEY (article_id, category_id),
+    FOREIGN KEY(article_id) References article,
+    FOREIGN KEY(category_id) References categories
+);
