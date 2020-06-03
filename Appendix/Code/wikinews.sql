47025e68f16911e24bf929a27212d47ad34885d1
Jonathan Ahrenkiel-Frellsen <jola190500@gmail.com>
2020-06-03 21:39:03 +0200

    Changed names for sql tables and files

--- Appendix/Code/wikinews.sql
+++ Appendix/Code/wikinews.sql
@@ -1,5 +1,5 @@
 /* entities eller hvad det nu hedder */
-CREATE TABLE article (
+CREATE TABLE wikinews_article (
     article_id int,
     content TEXT,
     title varchar(1000),
@@ -8,20 +8,20 @@ CREATE TABLE article (
     PRIMARY KEY (article_id)
 );
 
-CREATE TABLE sources (
+CREATE TABLE wikinews_sources (
     source_domain_id int,
     source_domain varchar(255),
     PRIMARY KEY (source_domain_id)
 );
 
-CREATE TABLE categories (
+CREATE TABLE wikinews_categories (
     category_id int,
     category varchar(1000),
     PRIMARY KEY (category_id)
 );
 
 /* relations */
-CREATE TABLE source_to (
+CREATE TABLE wikinews_source_to (
     article_id int,
     source_domain_id int,
     PRIMARY KEY (article_id, source_domain_id),
@@ -29,7 +29,7 @@ CREATE TABLE source_to (
     FOREIGN KEY(source_domain_id) References sources
 );
 
-CREATE TABLE in_category (
+CREATE TABLE wikinews_in_category (
     article_id Int,
     category_id int,
     PRIMARY KEY (article_id, category_id),
