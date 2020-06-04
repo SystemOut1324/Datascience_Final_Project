ed583141820386a1557bed38a6fc4b6c5a989365
Jonathan Ahrenkiel-Frellsen <jola190500@gmail.com>
2020-06-04 16:36:08 +0200

    Added orginize for wiki and other things

---
+++ Appendix/Code/combined.sql
@@ -0,0 +1,6 @@
+/* Drop SCHEMA TO START FRESH */
+DROP SCHEMA IF EXISTS combined CASCADE;
+
+/* Create schema make collection of database objects including tables, views, triggers, stored procedures, indexes, etc*/
+CREATE SCHEMA combined
+    
